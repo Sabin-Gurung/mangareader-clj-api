4aa7e7f3da3e6812da51a5d463487e468a803c4c
sgurung <Infinity01>
2021-08-29 14:00:33 +0200

    testing runner

--- test/mangareader_clj_api/core_test.clj
+++ test/mangareader_clj_api/core_test.clj
@@ -1,7 +1,14 @@
 (ns mangareader-clj-api.core-test
   (:require [clojure.test :refer :all]
+            [mangareader-clj-api.api.handler-should]
             [mangareader-clj-api.core :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 1 1))))
+
+(comment
+  (run-tests
+    'mangareader-clj-api.api.handler-should
+    )
+  )
