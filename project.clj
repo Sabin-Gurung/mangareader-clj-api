3db4a79f78ba6d98c00b9e18093e68cd461269d3
sgurung <Infinity01>
2021-08-29 11:33:59 +0200

    sorted dependencies

--- project.clj
+++ project.clj
@@ -3,16 +3,18 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[enlive "1.1.6"]
                  [http-kit "2.5.3"]
-                 [enlive "1.1.6"]
-                 [metosin/reitit "0.5.15"]
                  [metosin/jsonista "0.3.3"]
-                 [prismatic/schema "1.1.12"]
-                 [mock-clj "0.2.1"]
-                 [ring/ring-mock "0.4.0"]
-                 ]
+                 [metosin/reitit "0.5.15"]
+                 [org.clojure/clojure "1.10.1"]
+                 [prismatic/schema "1.1.12"]]
   :resource-paths ["resources"]
   :main ^:skip-aot mangareader-clj-api.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev     {:dependencies [[mock-clj "0.2.1"]
+                                      [ring/ring-mock "0.4.0"]]}
+             :prod    {}
+             }
+  )
