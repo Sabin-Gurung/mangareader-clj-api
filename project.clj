210ae0d48f260032566419c95a4b51743c82475a
sgurung <Infinity01>
2021-08-30 21:27:28 +0200

    added leinningen

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
+  :min-lein-version "2.0.0"
   :dependencies [[enlive "1.1.6"]
                  [http-kit "2.5.3"]
                  [metosin/jsonista "0.3.3"]
